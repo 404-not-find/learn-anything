eddd1beabdd09877044df389168515767344a916
John Letey <30328854+johnletey@users.noreply.github.com>
2019-10-11 15:22:28 +0100

    Add link to home in topic pages

--- src/templates/topic.js
+++ src/templates/topic.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 
 export const query = graphql`
   query($id: hasura_uuid!) {
@@ -17,6 +17,9 @@ export default ({ data }) => {
 
   return (
     <div style={{ margin: "5rem auto", width: "550px" }}>
+      <h3>
+        <Link to="/">Home</Link>
+      </h3>
       <h1>{topic.name}</h1>
     </div>
   );
