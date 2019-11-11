13ca08f113ed2db7679ead8b9756d87184badf08
Nikita Voloboev <nikita.voloboev@gmail.com>
2019-11-11 18:55:28 +0000

    serve all topics

--- api/main.go
+++ api/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("test")
+	http.HandleFunc("/maps", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./data/maps.json")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
